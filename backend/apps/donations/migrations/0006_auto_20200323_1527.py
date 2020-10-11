5e36438623003ca71cdd96f5b4f87995a91d7562
David Marabay <david.marabay@gmail.com>
2020-10-11 17:43:57 -0430

    conection postgresql

--- backend/apps/donations/migrations/0006_auto_20200323_1527.py
+++ backend/apps/donations/migrations/0006_auto_20200323_1527.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='event',
             name='user',
-            field=models.ForeignKey(default='user', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
--- backend/wonderhumans/settings/dev.py
+++ backend/wonderhumans/settings/dev.py
@@ -1,11 +1,14 @@
 from .base import * 
 
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPASSWORD'),
+        'HOST': config('DBHOST'), # set in docker-compose.yml
+        'PORT': 5432, # default postgres port
+    }
+}
 
 ALLOWED_HOSTS = ['*']
