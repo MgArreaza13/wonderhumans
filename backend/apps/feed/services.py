90526333639a2e0542bc1b54310170ff3f700600
David Marabay <david.marabay@gmail.com>
2020-11-02 17:58:19 -0430

    update and delete feed

--- backend/apps/feed/services.py
+++ backend/apps/feed/services.py
@@ -17,6 +17,7 @@ from apps.comments import models as comments_models
 
 # My validations
 from apps.accounts import validations as accounts_validations
+from apps.feed import validations as feed_validations
 
 # My serializers
 from apps.feed import serializers as feed_serializers
@@ -35,7 +36,7 @@ def create_feed(data:dict, user:accounts_models.User)->feed_models.Feed:
     if data.get("image") is None:
         raise ValueError(str(_("Image is required")))
     if data.get("description") is not None:
-        accounts_validations.validate_length("Description",data.get("description"),0,255)
+        accounts_validations.validate_length("Description",data.get("description"),0,65000)
     else:
         raise ValueError(str(_("Description is required")))
     with transaction.atomic():
@@ -119,4 +120,46 @@ def get_feed(id_feed:int, user:accounts_models.User)->feed_models.Feed:
             feed['i_commented'] = 'false'
     except feed_models.Feed.DoesNotExist as e:
         raise ValueError(str(_("The feed does not exist")))
+    return feed
+
+def delete_feed(id_feed:int, user:accounts_models.User):
+    """
+        Method to delete feed by user
+
+        :param id_feed: feed id
+        :type id_feed: int
+        :param user: user created feed and now want delete
+        :para user: accounts_models.User
+    """
+    try:
+        feed = feed_models.Feed.objects.get(id=id_feed, userRegisterer=user)
+        feed.delete()
+    except feed_models.Feed.DoesNotExist as e:
+        raise ValueError(str(_("Feed does not exist")))
+
+def update_feed(data:dict, user:accounts_models.User)->feed_models.Feed:
+    """
+        Method to updte feed by user
+
+        :param data: data to update in feed
+        :type data: dict
+        :param user: user who created the feed and now updates it
+        :type user: accounts_models.User
+        :raise: ValueError
+        :return: feed_models.Feed
+    """
+    if data.get("id") is not None:
+        feed_validations.validate_id(data.get("id"))
+    else:
+        raise ValueError(str(_("id feed is required")))
+    try:
+        feed = feed_models.Feed.objects.get(id=data.get("id"), userRegisterer=user)
+    except feed_models.Feed.DoesNotExist as e:
+        raise ValueError(str(_("You dont have permission to update this feed")))
+    if data.get("image") is not None:
+        feed.image = accounts_services.updateImage(data.get("image"))
+    if data.get("description") is not None:
+        accounts_validations.validate_length("Description",data.get("description"),0,65000)
+        feed.description = data.get("description")
+    feed.save()
     return feed
