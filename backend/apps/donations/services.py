54dcda9c29a1c36664636d30d1922d91ad10bce8
David Marabay <david.marabay@gmail.com>
2020-11-20 20:53:32 -0430

    changes in services donations

--- backend/apps/donations/services.py
+++ backend/apps/donations/services.py
@@ -7,6 +7,16 @@ def getDonations(id_homeless: int):
     donations = donations_models.Donation.objects.filter(event__homeless__id = id_homeless)
     return donations
 
+def create_donation(data:dict):
+	"""
+		Method to create a new donation
+
+		:param data: content mont donation to some event
+		:type data: dict
+		:raise: ValueError
+		:return:
+	"""
+	pass
 
 def getEvents(id_homeless: int):
     events = donations_models.Event.objects.filter(homeless__id = id_homeless)
